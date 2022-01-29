bb6cd875996a1a564d867fdefb0907e8fa949c84
Moamen236 <moamen.ali107@gmail.com>
2022-01-29 17:14:46 +0200

    db

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\admin\UserController;
-use App\Http\Controllers\RedirectController;
 use App\Http\Controllers\web\VoteController;
 use Illuminate\Support\Facades\Route;
 
